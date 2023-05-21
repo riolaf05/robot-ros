fc75fd3f7faa619ac9ebcf0855d5480e8dfaec5b
Rosario Laface <lafacerosario@gmail.com>
2023-05-21 16:02:06 +0200

    update rsp scripts

--- build/robot_ros/prefix_override/sitecustomize.py
+++ build/robot_ros/prefix_override/sitecustomize.py
@@ -1,3 +1,3 @@
 import sys
 sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/ubuntu/robot-ros/install/robot_ros'
+sys.prefix = sys.exec_prefix = '/home/rosario/robot-ros/install/robot_ros'
